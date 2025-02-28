4e276209e6a4754cf12779bf226a6eff65a6b402
anderbelluno <36983422+anderbelluno@users.noreply.github.com>
2025-02-28 19:26:42 -0300

    Changes

--- sample/D12/Project1.dpr
+++ sample/D12/Project1.dpr
@@ -4,11 +4,11 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  Badger in '..\TTCPBlockSocket\src\Badger.pas',
-  BadgerMethods in '..\TTCPBlockSocket\src\BadgerMethods.pas',
-  BadgerMultipartDataReader in '..\TTCPBlockSocket\src\BadgerMultipartDataReader.pas',
-  BadgerRouteManager in '..\TTCPBlockSocket\src\BadgerRouteManager.pas',
-  SyUtils in '..\TTCPBlockSocket\src\SyUtils.pas';
+  Badger in '..\..\src\Badger.pas',
+  BadgerMethods in '..\..\src\BadgerMethods.pas',
+  BadgerMultipartDataReader in '..\..\src\BadgerMultipartDataReader.pas',
+  BadgerRouteManager in '..\..\src\BadgerRouteManager.pas',
+  SyUtils in '..\..\src\SyUtils.pas';
 
 {$R *.res}
 
